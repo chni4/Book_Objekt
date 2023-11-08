6a56b117312d528f0d025d49fbfa9cd510c19fb5
chni4 <149565672+chni4@users.noreply.github.com>
2023-11-08 16:29:39 +0100

    Add files via upload

---
+++ src/main/java/org/example/Library.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Library {
+                public void Libr() {
+                    Book[] lib = new Book[3];
+                    lib[0] = 
+                    lib[1] =
+                    lib[2] =
+}
+
+
+}
